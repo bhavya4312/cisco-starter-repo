bb57c3048ac4eb975a9a15b260100c3813a10725
Bhavya Vora <bhavyavora0@gmail.com>
2022-11-21 05:56:00 +0530

    2nd Assignment

---
+++ sextant/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from './Banner';
+import Card from './Card';
+
+function App() { 
+    return (
+      <div className="App">
+        <Banner />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
+}
+
+
+export default App;
---
+++ sextant/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Card() {
+    return (
+      <div className="note">
+        <h1>Lorem Ipsum</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae quia magnam eligendi dignissimos eos repellendus inventore deserunt magni ex perferendis sunt voluptatum temporibus impedit, nam fuga aperiam? Quos, facilis iusto.</p>
+      </div>
+    );
+}
+
+export default Card;
