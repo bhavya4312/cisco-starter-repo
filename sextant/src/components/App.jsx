6a2ae9ff07c626e0d372dd3a13d1f593a53c3809
Bhavya Vora <bhavyavora0@gmail.com>
2022-11-22 16:31:09 +0530

    Final Commit

--- sextant/src/components/App.jsx
+++ sextant/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner';
 import Exhibit from './Exhibit';
 import Address from './Address';
+import Pylon from './Pylon';
 
 function App() { 
     return (
@@ -14,6 +15,9 @@ function App() {
         </Exhibit>  
         <Exhibit heading="Public IPv6 Address">
             <Address url='https://api64.ipify.org?format=json' />
+        </Exhibit>
+        <Exhibit heading="Packet Latency">
+            <Pylon />
         </Exhibit>
 
       </div>
